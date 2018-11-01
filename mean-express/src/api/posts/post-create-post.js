fff3fe97152ba845f27c3631002e6a64f1cf210d
rodrigo.a.lopes <rodrigo.a.lopes@itamaraty.gov.br>
2018-11-01 17:32:27 -0300

    finish course

--- mean-express/src/api/posts/post-create-post.js
+++ mean-express/src/api/posts/post-create-post.js
@@ -4,7 +4,6 @@ const slugify = require('slugify');
 module.exports = async (req, res) => {
     req.body.slug = slugify(req.body.title, {lower: true});
     const post = new Post(req.body);
-
     try {
         const saved = await post.save();
         return res.status(201).json({post: saved});
